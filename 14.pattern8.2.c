db0754bb2fdb72e6c9cadc8fe0088fcc84b9d4e1
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2024-01-03 22:42:56 +0530

    Add files via upload

---
+++ 14.pattern8.2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        for(j=n-1;j>=0;j--){
+            if(i+j==n-1||i==j)
+            printf("*");
+            else
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
