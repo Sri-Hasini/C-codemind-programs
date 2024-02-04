f30f061591f0e9a364cd0e1550adf3f0a543c7e5
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2024-02-04 15:33:19 +0530

    Add files via upload

---
+++ 7.months.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int main()
+{
+	int m;
+	scanf("%d",&m);
+	if(m==1)
+	{
+		printf("JANUARY");
+	}
+	else if(m==2)
+	{
+		printf("FEBRUARY");
+	}
+	else if(m==3)
+	{
+		printf("MARCH");
+	}
+	else if(m==4)
+	{
+		printf("APRIL");
+	}
+	else if(m==5)
+	{
+		printf("MAY");
+	}
+	else if(m==6)
+	{
+		printf("JUNE");
+	}
+	else if(m==7)
+	{
+		printf("JULY");
+	}
+	else if(m==8)
+	{
+		printf("AUGUST");
+	}
+	else if (m==9)
+	{
+		printf("SEPTEMBER");
+	}
+	else if (m==10)
+	{
+		printf("OCTOBER");
+	}
+	else if (m==11)
+	{
+		printf("NOVEMBER");
+	}
+	else if (m==12)
+	{
+		printf("DECEMBER");
+	}
+	else 
+	{
+		printf("DON'T YOU NOW THAT THERE ARE ONLY 12 MONTHS!");
+	}
+}
