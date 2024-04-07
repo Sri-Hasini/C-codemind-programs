8bf36ad729d8f03cb44a9324111c5a227dd68d14
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2024-04-07 14:15:14 +0530

    Add files via upload

---
+++ 52.colwisesum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n,m,i,j;
+    scanf("%d%d",&n,&m);
+    int a[n][m],sum[n];
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++){
+        k=0;
+        for(j=0;j<m;j++){
+            while(k<n{
+            sum[k]+=a[0][i];
+            }
+        }
+        k++;
+    }
+}
